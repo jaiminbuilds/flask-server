e63e21f229ada0350b90cacdfe2883d7ea62e663
jaiminbuilds <jaimin@caddifi.com>
2023-12-03 04:29:13 +0000

    Add message endpoint

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/message')
+def send_message():
+    return 'This is a message from the server.'
+
 if __name__ == '__main__':
     app.run(debug=True)
+
