bb38c75f4d050551318f32f025a189d3469c327a
jaiminbuilds <jaimin@caddifi.com>
2023-12-03 04:57:11 +0000

    Add message endpoint

--- app.py
+++ app.py
@@ -11,5 +11,7 @@ def send_message():
     return 'This is a message from the server.'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
 
